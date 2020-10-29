c2a86f7713b8c64181334a332f146069ac449bd5
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2020-10-29 15:25:56 -0400

    added function to buttons

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,61 +1,60 @@
 <template>
   <div id="app">
-    <Header msg="trees App"/>
-    <Location msg='' v-on:setSearch='setSearch'/>
-    <Species msg=''  v-on:setSearch='setSearch'/>
-    <Diameter msg=''  v-on:setSearch='setSearch'/>
-    <SearchBar  v-bind:trees="this.baseTrees" msg="location"/>
+    <Header msg="trees App" />
+    <Species msg v-on:setSearch="setSearch" />
+    <Location msg v-on:setSearch="setSearch" />
+    <Diameter msg v-on:setSearch="setSearch" />
+    <SearchBar v-bind:search="this.baseSearch" msg="location" />
   </div>
 </template>
 
 <script>
-import Header from './components/header.vue'
-import Location from './components/location'
-import Diameter from './components/diameter'
-import SearchBar from './components/SearchBar'
-import Species from './components/species'
+import Header from "./components/header.vue";
+import Location from "./components/location";
+import Diameter from "./components/diameter";
+import SearchBar from "./components/SearchBar";
+import Species from "./components/species";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
     Location,
     Diameter,
     SearchBar,
     Species
-
   },
   methods: {
-   
-   setSearch(x) {
-      console.log('set search', x)
+    setSearch(x) {
+      console.log("set search", x);
+      if (x === 'Species') {
+          console.log('species')
+          return this.baseSearch = x; 
+
+      } else if(  x === 'Location') {
+          console.log('local')
+
+          return this.baseSearch = x; 
+
+      } else {
+          console.log('kick a doo')
+
+        return this.baseSearch = x; 
+
+      }
     },
 
-    getTrees() {
-       console.log('hi   errret');
-   
-      // e.preventDefault();
- this.$http.plain.get('/trees')
-
-.then( response => this.baseTrees = response.data)
-  .then(() => console.log(this.baseTrees))
-  .catch(err => console.log(":(",err))
-
- console.log(this.baseTrees)
-}
   },
-  created () {
-    this.getTrees();
+  created() {
+    // this.getTrees();
   },
   data() {
     return {
-      baseTrees: [
-
-      ]
-    }
+      baseTrees: [],
+      baseSearch: "Species"
+    };
   }
-
-}
+};
 </script>
 
 <style>
--- client/src/components/SearchBar.vue
+++ client/src/components/SearchBar.vue
@@ -1,18 +1,20 @@
 <template>
-<div>
-  <!-- <h2>{{msg}}</h2> -->
-   <form @submit="searchTree">
-     <select name="tree-selector" >
-<!-- <div v-bind:key="tree.x" v-for="tree in trees"> -->
-  <option v-bind:key="tree.x" v-for="tree in trees" value="tree.x">
-   {{tree.common_name}}
-    </option>
-  <!-- </div> -->
-</select>
-  <input type="text" :placeholder="this.placeHolderValue"/>
-    <input type="submit" value="submitboi" class='btn' @submit.prevent="searchTree"/>
-  </form>
-</div>
+  <div>
+    <!-- <h2>{{msg}}</h2> -->
+    <form @submit="searchTree">
+      <select  v-if="this.search ===  'Species'" >
+        <option v-bind:key="tree.x" v-for="tree in trees" value="tree.x">{{tree.common_name}} {{tree.botanical_name}}</option>
+      </select>      
+       <select v-else-if="this.search ===  'Location'">
+        <option v-bind:key="tree.y" v-for="tree in trees" value="tree.y">{{tree.address}} {{tree.name}} </option>
+      </select>      
+        <select v-else name="tree-selector">
+        <option  v-bind:key="tree.y" v-for="tree in trees" value="tree.x">{{tree.dbh_trunk}}</option>
+      </select>
+      <input type="text" :placeholder="this.search" />
+      <input type="submit" value="submitboi" class="btn" @submit.prevent="searchTree" />
+    </form>
+  </div>
 </template>
 
 <script>
@@ -22,29 +24,43 @@ export default {
   name: "SearchBar",
   data() {
     return {
-      placeHolderValue: 'weee'
-    }
+      trees: [],
+      dataSelect: 'Location'
+    };
   },
   props: {
-    trees: Array
+    search: String,
+    select: String
   },
   methods: {
-     searchTree(e) {
-      console.log('hit');
-   
-      e.preventDefault();
- this.$http.plain.get('/trees')
-  .then( response => response.data)
-  .then(data => console.log(data[0]))
-  .catch(err => console.log(":(",err))
+    searchTree(e) {
+      console.log("hit");
 
- }
-},
-created(){
-  console.log(this.trees)
-}
-    
-  
+      e.preventDefault();
+      this.$http.plain
+        .get("/trees")
+        .then(response => response.data)
+        .then(data => console.log(data[0]))
+        .catch(err => console.log(":(", err));
+    },
+    getTrees() {
+      console.log("hi   errret");
+      this.$http.plain
+        .get("/trees")
+
+        .then(response => (this.trees = response.data))
+        .then(() => console.log(this.trees))
+        .catch(err => console.log(":(", err));
+
+      console.log(this.baseTrees);
+    }
+  },
+
+  created() {
+    this.getTrees();
+    console.log(this.trees);
+    this.dataSelect =  this.search;
+  }
 };
 </script>
 
